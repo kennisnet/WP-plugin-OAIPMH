7b30f113a1ddce5a7fb5febb6d0434120008d365
Ramon Fincken <r.fincken@kennisnet.nl>
2020-07-10 11:26:27 +0200

    [TASK] version

--- wpoaipmh.php
+++ wpoaipmh.php
@@ -5,7 +5,7 @@
 * Plugin URI:		https://www.kennisnet.nl
 * Description:		This plugin connects the WordPress content with OAIPMH
 *
-* Version:         2.1.2
+* Version:         2.1.3
 *
 * @link       https://www.kennisnet.nl
 * @since      1.0.0
